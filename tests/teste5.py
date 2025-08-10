6060dfff8f86da78f8ea760bf397183f5200eded
Dinah <dinah.navas@gmail.com>
2025-08-10 18:33:42 -0300

    revertendo atualizações

---
+++ tests/teste5.py
@@ -0,0 +1,11 @@
+#Teste 5: Mede latência das operações
+from locust import HttpUser, task, between
+
+class LatencyTestUser(HttpUser):
+    wait_time = between(0.5, 1)
+
+    @task
+    def get_user_by_id(self):
+        user_id = 1  
+        response = self.client.get(f"/usuarios/{user_id}")
+        print(f"Status: {response.status_code}")
